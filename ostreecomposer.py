f3b8deb5330e7a026df7229dda61d25e43fcbbeb
Luke Macken <lmacken@redhat.com>
2014-10-19 13:11:29 -0600

    Better logging

--- ostreecomposer.py
+++ ostreecomposer.py
@@ -16,18 +16,18 @@ class OSTreeComposer(fedmsg.consumers.FedmsgConsumer):
         topic = body['topic']
 
         if 'rawhide' in topic:
-            self.log.info('New rawhide compose ready')
             arch = body['msg']['arch']
+            self.log.info('New rawhide %s compose ready', arch)
         elif 'branched' in topic:
-            self.log.info('New branched compose ready')
             arch = body['msg']['arch']
             branch = body['msg']['branch']
+            self.log.info('New %s %s branched compose ready', branch, arch)
             log = body['msg']['log']
             if log != 'done':
                 self.log.warn('Compose not done?')
                 return
         elif 'updates' in topic:
-            self.log.info('New updates compose ready')
+            self.log.info('New %s %s compose ready', release, repo)
             release = body['msg']['release']
             repo = body['msg']['repo']
 
