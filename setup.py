9620eb2417eaecb4ae4c88f6205a4c7ee5f2e2d9
Luke Macken <lmacken@redhat.com>
2014-10-20 10:35:14 -0600

    Update the setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name='ostreecomposer',
     version='0.0.1',
-    description='',
-    author='',
-    author_email='',
+    license='GPLv3',
+    description='Composes OSTrees when Fedora repositories are updated',
+    author='Luke Macken',
+    author_email='lmacken@redhat.com',
     url='',
     install_requires=["fedmsg"],
     packages=[],
