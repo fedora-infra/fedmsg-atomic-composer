20d3d75f8587641a7f3163344441778571df5409
Luke Macken <lmacken@redhat.com>
2014-11-18 12:34:55 -0700

    build: Make the build script update the rpms and run ansible

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
-python setup.py sdist --format=bztar
-mv dist/* ~/rpmbuild/SOURCES/
-cp rpm/*.spec ~/rpmbuild/SPECS/
-rpmbuild -ba ~/rpmbuild/SPECS/fedmsg-atomic-composer.spec
+#!/bin/bash -x
+python setup.py sdist --format=bztar && \
+mv dist/* ~/rpmbuild/SOURCES/ && \
+cp rpm/*.spec ~/rpmbuild/SPECS/ && \
+rpmbuild -ba ~/rpmbuild/SPECS/fedmsg-atomic-composer.spec && \
+sudo rpm -e fedmsg-atomic-composer{,-consumer} ; \
+sudo rpm -vih ~/rpmbuild/RPMS/noarch/fedmsg-atomic-composer{,-consumer}-0.0.1-2.fc21.noarch.rpm && \
+ansible-playbook -v ansible/playbook.yml
