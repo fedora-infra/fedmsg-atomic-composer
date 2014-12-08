d4b0679b50b6d26a783dba5c5b78b6469e33b2c7
Luke Macken <lmacken@redhat.com>
2014-12-08 11:17:12 -0500

    build.sh improvements

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
+PACKAGE=fedmsg-atomic-composer
+VERSION=`grep ^Version rpm/$PACKAGE.spec | cut -d: -f2 | tr -d ' '`
+RELEASE=`grep ^Release rpm/$PACKAGE.spec | cut -d: -f2 | cut -d% -f1 | tr -d ' '`
 python setup.py sdist --format=bztar && \
 mv dist/* ~/rpmbuild/SOURCES/ && \
 cp rpm/*.spec ~/rpmbuild/SPECS/ && \
-rpmbuild -ba ~/rpmbuild/SPECS/fedmsg-atomic-composer.spec && \
-sudo rpm -e fedmsg-atomic-composer{,-consumer} ; \
-sudo rpm -vih ~/rpmbuild/RPMS/noarch/fedmsg-atomic-composer{,-consumer}-0.0.1-2.fc21.noarch.rpm && \
+rpmbuild -ba ~/rpmbuild/SPECS/$PACKAGE.spec && \
+sudo rpm -e $PACKAGE{,-consumer} ; \
+sudo rpm -vih ~/rpmbuild/RPMS/noarch/$PACKAGE{,-consumer}-$VERSION-$RELEASE.*.noarch.rpm
 ansible-playbook -v ansible/playbook.yml
