bb3e68d205368f05c9ffed1df865b9e6e814c187
Luke Macken <lmacken@redhat.com>
2014-12-18 11:08:50 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fedmsg-atomic-composer',
-    version='2014.2',
+    version='2014.3',
     license='GPLv3',
     description='Composes Atomic rpm-ostrees when Fedora repositories are updated',
     author='Luke Macken',
