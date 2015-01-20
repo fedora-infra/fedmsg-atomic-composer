3d9da165768e6df65c0a4e3847e96061c04ef054
Luke Macken <lmacken@redhat.com>
2015-01-19 18:15:00 -0700

    config: Add a header

--- fedmsg_atomic_composer/config.py
+++ fedmsg_atomic_composer/config.py
@@ -1,3 +1,26 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+The primary atomic composer configuration.
+
+The appropriate entry in the 'releases' must be passed into
+the AtomicComposer.composer method.
+
+The code at the bottom maps+expands the paths &
+commands to each of the releases, for ease of
+configuration.
+"""
+
 # Check if we're running on RHEL6 and disable
 # `mock --new-chroot` and `rpm-ostree --workdir-tmpfs`
 import platform
